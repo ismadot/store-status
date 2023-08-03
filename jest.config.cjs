c4f654251a89f52bcb6718421d40945f573ff3fd
Ismael Celis <ismaelcelis@Ismaels-Mini.lan>
2023-08-03 14:16:22 -0400

    add configs to test

---
+++ jest.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  moduleNameMapper: {
+    "\\.(css|scss)$": "identity-obj-proxy",
+  },
+};
