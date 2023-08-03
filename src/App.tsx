b9e5df47b876b92f61ee1861c678119af1b6ffc7
Ismael Celis <ismaelcelis@Ismaels-Mini.lan>
2023-08-03 13:37:32 -0400

    add view

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import { Store } from "./Store"; // Import the Store class
+
+type IncidentStatus = {
+  openCases?: number;
+  closedCases?: number;
+  averageSolutionTime?: number;
+  maximumSolutionTime?: number;
+};
+
+type Incident = {
+  id: number;
+  description: string;
+  status: string;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [store] = useState(new Store());
+  const [incidents, setIncidents] = useState<Incident[]>([]);
+  const [newIncidentDescription, setNewIncidentDescription] = useState("");
+  const [incidentIdToSolve, setIncidentIdToSolve] = useState("");
+  const [incidentStatus, setIncidentStatus] = useState<IncidentStatus>({});
+
+  useEffect(() => {
+    setIncidents(store.getAllIncidentDescriptions());
+    const from = new Date();
+    from.setHours(from.getHours() - 24); // 24 hours ago
+    const to = new Date(); // Now
+    setIncidentStatus(store.incidentStatus(from, to));
+  }, [store]);
+
+  const reportIncident = () => {
+    store.reportIncident(newIncidentDescription);
+    setIncidents(store.getAllIncidentDescriptions());
+    setNewIncidentDescription("");
+  };
+
+  const solveIncident = () => {
+    store.solveIncident(Number(incidentIdToSolve));
+    setIncidents(store.getAllIncidentDescriptions());
+    setIncidentIdToSolve("");
+  };
 
   return (
-    <>
+    <div className="App">
+      <h1>Store Incident Management</h1>
+
+      <input
+        type="text"
+        value={newIncidentDescription}
+        onChange={(e) => setNewIncidentDescription(e.target.value)}
+        placeholder="New incident description"
+      />
+      <button onClick={reportIncident}>Report Incident</button>
+
+      <input
+        type="text"
+        value={incidentIdToSolve}
+        onChange={(e) => setIncidentIdToSolve(e.target.value)}
+        placeholder="Incident ID to solve"
+      />
+      <button onClick={solveIncident}>Solve Incident</button>
+
+      <h2>Incidents</h2>
+      <ul>
+        {incidents.map((incident) => (
+          <li key={incident.id}>
+            <h3>
+              <strong>Id:</strong>
+              {` ${incident.id} - `}
+              <strong>Status: </strong>
+              {` ${incident.status} - `}
+              <strong>Description: </strong>
+              {` ${incident.description} `}
+            </h3>
+          </li>
+        ))}
+      </ul>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Incident Status in the Last 24 Hours</h2>
+        <p>Open Cases: {incidentStatus.openCases}</p>
+        <p>Closed Cases: {incidentStatus.closedCases}</p>
+        <p>Average Solution Time: {incidentStatus.averageSolutionTime} hours</p>
+        <p>Maximum Solution Time: {incidentStatus.maximumSolutionTime} hours</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
